d624247963a5234e8a6df29eafe172cec2999165
kondyurin <a.kondyurin@yandex.ru>
2019-04-30 12:39:58 +0300

    Update get_image.py

--- get_image.py
+++ get_image.py
@@ -6,13 +6,14 @@ import shutil
 directory = 'images'
 
 
-def fetch_img(url,path):
+def fetch_img(url, path):
     if not os.path.exists(directory):
         os.makedirs(directory)
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        with open(path, 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    response = requests.get(url, stream=True)
+    if not response.ok:
+        return
+    with open(path, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
 
 
